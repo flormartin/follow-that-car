5e1e16cc12c54dab77047a5343b61af757c390ed
Fuyuan Li <ga26piq@mytum.de>
2018-06-01 14:03:27 +0200

    Finish implementation OnClickListener of FloatingActionButton

--- app/src/main/java/de/tum/mw/ftm/followthatcar/MainActivity.java
+++ app/src/main/java/de/tum/mw/ftm/followthatcar/MainActivity.java
@@ -12,10 +12,12 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     private FloatingActionButton fab;
+    private FrameLayout container;
+    private boolean isServiceRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,36 +26,57 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        getFragmentManager().beginTransaction().add(R.id.container, new InputFragment()).commit();
+        final InputFragment inputFragment = new InputFragment();
+        getFragmentManager().beginTransaction().add(R.id.container, inputFragment).commit();
+        container = findViewById(R.id.container);
 
         fab = findViewById(R.id.fab);
         //TODO: function of floating action button
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setFabIcon(true);
-                FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(0, FrameLayout.LayoutParams.MATCH_PARENT);
-                params.gravity = 0;
-                try {
-                    blankFragment.getView().setLayoutParams(params);
-                }catch (NullPointerException e) {
-                    Log.e(TAG, "onClick: ", e);
+                if (!isServiceRunning) {
+                    setFabIcon(true);
+                    // Set view away
+                    moveContainerAway();
+                    isServiceRunning = true;
+                } else {
+                    setFabIcon(false);
+                    // Set view away
+                    getContainerBack();
+                    isServiceRunning = false;
                 }
+
+//                FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(0, FrameLayout.LayoutParams.MATCH_PARENT);
+//                params.gravity = 0;
+//                try {
+//                    getFragmentManager().beginTransaction().remove(inputFragment).commit();
+//                }catch (NullPointerException e) {
+//                    Log.e(TAG, "onClick: ", e);
+//                }
 
             }
         });
     }
 
+    private void moveContainerAway() {
+        container.setVisibility(View.GONE);
+    }
+
+    private void getContainerBack() {
+        container.setVisibility(View.VISIBLE);
+    }
+
     /**
      * Sets the correct icon for the floating action button
+     *
      * @param isServiceRunning if the Service is running
      */
-    private void setFabIcon(boolean isServiceRunning){
-        if(isServiceRunning){
+    private void setFabIcon(boolean isServiceRunning) {
+        if (isServiceRunning) {
             //If service is running set the stop icon
             fab.setImageResource(R.drawable.ic_stop);
-        }
-        else {
+        } else {
             //If service is not running set the start icon
             fab.setImageResource(R.drawable.ic_start);
         }
@@ -64,7 +87,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
-    public void addFab(){
+    public void addFab() {
         FloatingActionButton floatingActionButton = new FloatingActionButton(this);
     }
 }
