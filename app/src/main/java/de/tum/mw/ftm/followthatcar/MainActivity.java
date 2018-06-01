e5714083326b3914742f0250b6e8b170e271f0ba
Fuyuan Li <ga26piq@mytum.de>
2018-06-01 11:43:11 +0200

    Finish implentation of FloatingActionButton TODO: Function of FloatingActionButton

--- app/src/main/java/de/tum/mw/ftm/followthatcar/MainActivity.java
+++ app/src/main/java/de/tum/mw/ftm/followthatcar/MainActivity.java
@@ -1,13 +1,21 @@
 package de.tum.mw.ftm.followthatcar;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +24,48 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        getFragmentManager().beginTransaction().add(R.id.blank, new BlankFragment()).commit();
+        final BlankFragment blankFragment = new BlankFragment();
+        getFragmentManager().beginTransaction().add(R.id.blank, blankFragment).commit();
 
+        fab = findViewById(R.id.fab);
+        //TODO: function of floating action button
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setFabIcon(true);
+                FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(0, FrameLayout.LayoutParams.MATCH_PARENT);
+                params.gravity = 0;
+                try {
+                    blankFragment.getView().setLayoutParams(params);
+                }catch (NullPointerException e) {
+                    Log.e(TAG, "onClick: ", e);
+                }
+
+            }
+        });
+    }
+
+    /**
+     * Sets the correct icon for the floating action button
+     * @param isServiceRunning if the Service is running
+     */
+    private void setFabIcon(boolean isServiceRunning){
+        if(isServiceRunning){
+            //If service is running set the stop icon
+            fab.setImageResource(R.drawable.ic_stop);
+        }
+        else {
+            //If service is not running set the start icon
+            fab.setImageResource(R.drawable.ic_start);
+        }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
+    }
+
+    public void addFab(){
+        FloatingActionButton floatingActionButton = new FloatingActionButton(this);
     }
 }
